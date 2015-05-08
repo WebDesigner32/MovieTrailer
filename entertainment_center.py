ecd05c00e467b9306f2f1614050e4f04d5c89ccc
Landon Tyler Bennett <landonepgy@gmail.com>
2015-05-08 14:52:31 -0700

    Cleaned the code of useless jargon.

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,18 +5,13 @@ toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-#print(toy_story.storyline)
 avatar = media.Movie("Avatar", "A marine on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
-#print(avatar.storyline)
-#avatar.show_trailer()
-
 school_of_rock = media.Movie("School of Rock", "Using rock music to learn",
                              "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
-
 
 ratatouille = media.Movie("Ratatouille", "A rat is a chef in Paris",
                           "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
@@ -32,5 +27,3 @@ hunger_games = media.Movie("Hunger Games", "A really real reality show",
 
 movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
 fresh_tomatoes.open_movies_page(movies)
-print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
