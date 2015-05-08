d9268e26ca33d464fcacb75d570312611e1a725f
Landon Tyler Bennett <landonepgy@gmail.com>
2015-05-08 16:27:51 -0700

    To be completely Udacious or exceed expectations, I edited the HTML to my liking.

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -6,7 +6,7 @@ import re
 main_page_head = '''
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>Landon's Movie Trailers!</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -86,7 +86,7 @@ main_page_head = '''
 main_page_content = '''
 <!DOCTYPE html>
 <html lang="en">
-  <body>
+  <body style="background-color:orange">
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
       <div class="modal-dialog">
@@ -105,7 +105,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Landon's Movie Trailers</a>
           </div>
         </div>
       </div>
